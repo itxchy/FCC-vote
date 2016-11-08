cb00a25f62e6939800abfc8a6174f18f83ad2b64
Matt Trifilo <matt.trifilo@gmail.com>
2016-11-08 14:27:58 -0600

    add mock redux store and fix edit button test

--- __test__/CreateAPoll.spec.js
+++ __test__/CreateAPoll.spec.js
@@ -2,6 +2,7 @@ const React = require('react')
 const { mount } = require('enzyme')
 const { Provider } = require('react-redux')
 const { store } = require('../redux/Store.jsx')
+const configureMockStore = require('redux-mock-store')
 const NewPollTitle = require('../components/CreateAPoll/NewPollTitle.jsx')
 
 describe('CreateAPoll', () => {
@@ -12,12 +13,33 @@ describe('CreateAPoll', () => {
         )
       expect(wrapper.find('.new-poll-title-textarea').length).toEqual(1)
     })
-    it('should show a saved poll title upon receiving titleEditable prop', () => {
+    it('should show a saved poll title when the save icon is clicked', () => {
       const wrapper = mount(
           <NewPollTitle store={store} />
         )
       wrapper.find('.save-icon').simulate('click')
-      expect(wrapper.find('.edit-icon').length).toEqual(1)
+      expect(wrapper.find('.saved-title').length).toEqual(1)
+    })
+    it('should show a text area again if the edit button is clicked', () => {
+      // Set up mock store
+      const mockStore = configureMockStore.default()
+      let initialState = {
+        newPollTitle: 'blah',
+        titleEditable: false,
+        newPollOptions: [
+          '',
+          ''
+        ]
+      }
+      let testStore = mockStore(initialState)
+
+      // Set up test 
+      const wrapper = mount(
+          <NewPollTitle store={testStore} />
+        )
+      wrapper.find('.edit-icon').simulate('click')
+      expect(wrapper.find('.new-poll-title-textarea'))
+
     })
   })
 })
