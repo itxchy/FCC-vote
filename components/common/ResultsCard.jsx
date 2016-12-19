f8fa40703e7486f737262ccdf8a0f0c800c32a63
Matt Trifilo <matt.trifilo@gmail.com>
2016-12-18 20:47:27 -0600

    add ResultsCard

---
+++ components/common/ResultsCard.jsx
@@ -0,0 +1,26 @@
+const React = require('react')
+const { connector } = require('../../redux/Store')
+const { string, array, number, object, func } = React.PropTypes
+
+const ResultsCard = React.createClass({
+  propTypes: {
+    title: string,
+    options: array,
+    totalVotes: number,
+    id: string,
+    user: object
+  },
+  render () {
+    return (
+      <div className='col-sm-4'>
+        <h2>{this.props.title}</h2>
+        <div className='col-sm-10'>
+          {/* options */}
+        </div>
+        <p>Total votes: {this.props.totalVotes}</p>
+      </div>
+    )
+  }
+})
+
+module.exports = connector(ResultsCard)
