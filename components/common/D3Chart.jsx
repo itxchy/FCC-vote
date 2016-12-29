5a843f17f9b816835114d1ce4fc58017b5a1b9f1
Matt Trifilo <matt.trifilo@gmail.com>
2016-12-28 19:57:35 -0600

    add svg rectangles to poll cards

--- components/common/D3Chart.jsx
+++ components/common/D3Chart.jsx
@@ -10,17 +10,47 @@ const D3Chart = React.createClass({
   },
   render () {
     let chart = ReactFauxDom.createElement('div')
-    console.log('results:', this.props.results)
+    console.log('data:', this.props.results)
+    console.log('data.length:', this.props.results.length)
+    // d3.select(chart)
+    //   .selectAll('p')
+    //   .data(this.props.results)
+    //   .enter()
+    //   .append('p')
+    //   .text(d => {
+    //     console.log('d', d)
+    //     return d.option + ' ' + d.votes.length
+    //   })
+    let data = this.props.results
+    var width = 200
+    var height = 500
 
-    d3.select(chart)
-      .selectAll('p')
-      .data(this.props.results)
+    var svg = d3.select(chart)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+
+    svg.selectAll('rect')
+      .data(data)
       .enter()
-      .append('p')
-      .text(d => {
-        console.log('d', d)
-        return d.option + ' ' + d.votes.length
-      })
+      .append('rect')
+      .attr('x', 10)
+      .attr('y', (d, i) => i * (height/data.length))
+      .attr('width', d => d.votes.length * 4)
+      .attr('height', d => height/data.length - 8)
+      .attr('fill', 'teal')
+
+    // svg.selectAll('text')
+    //   .data(this.props.results)
+    //   .enter()
+    //   .append('text')
+    //   .text(d => d.option + ': ' + d.votes)
+    //   .attr('x', 16)
+    //   .attr('y', (d,i) => i * (height/data.length) + 24)
+    //   .attr('width', d => d.votes * 4)
+    //   .attr('height', d => height/data.length - 8)
+    //   .attr("font-family", "sans-serif")
+    //   .attr("font-size", 18)
 
     console.log('faux element:', chart)
     return chart.toReact()
--- public/bundle.js
+++ public/bundle.js
@@ -76256,12 +76256,42 @@
 	  },
 	  render: function render() {
 	    var chart = ReactFauxDom.createElement('div');
-	    console.log('results:', this.props.results);
+	    console.log('data:', this.props.results);
+	    console.log('data.length:', this.props.results.length);
+	    // d3.select(chart)
+	    //   .selectAll('p')
+	    //   .data(this.props.results)
+	    //   .enter()
+	    //   .append('p')
+	    //   .text(d => {
+	    //     console.log('d', d)
+	    //     return d.option + ' ' + d.votes.length
+	    //   })
+	    var data = this.props.results;
+	    var width = 200;
+	    var height = 500;
 	
-	    d3.select(chart).selectAll('p').data(this.props.results).enter().append('p').text(function (d) {
-	      console.log('d', d);
-	      return d.option + ' ' + d.votes.length;
-	    });
+	    var svg = d3.select(chart).append('svg').attr('width', width).attr('height', height);
+	
+	    svg.selectAll('rect').data(data).enter().append('rect').attr('x', 10).attr('y', function (d, i) {
+	      return i * (height / data.length);
+	    }).attr('width', function (d) {
+	      return d.votes.length * 4;
+	    }).attr('height', function (d) {
+	      return height / data.length - 8;
+	    }).attr('fill', 'teal');
+	
+	    // svg.selectAll('text')
+	    //   .data(this.props.results)
+	    //   .enter()
+	    //   .append('text')
+	    //   .text(d => d.option + ': ' + d.votes)
+	    //   .attr('x', 16)
+	    //   .attr('y', (d,i) => i * (height/data.length) + 24)
+	    //   .attr('width', d => d.votes * 4)
+	    //   .attr('height', d => height/data.length - 8)
+	    //   .attr("font-family", "sans-serif")
+	    //   .attr("font-size", 18)
 	
 	    console.log('faux element:', chart);
 	    return chart.toReact();
