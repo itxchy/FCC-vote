499b4deca9ac5858a507916d51f32f1a95eb932b
Matt Trifilo <matt.trifilo@gmail.com>
2017-01-17 11:01:30 -0600

    add DisplayPolls

---
+++ components/common/DisplayPolls.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import ResultsCard from './ResultsCard'
+import PollCard from './PollCard'
+const { func } = React.PropTypes
+
+const DisplayPolls = React.createClass({
+  render() {
+    return (
+      <div className='row'>
+      </div>
+    )
+  }
+})
