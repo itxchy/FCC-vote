488413346c7cac0b3f160c28a979ecfb9b264b08
Matt Trifilo <matt.trifilo@gmail.com>
2017-02-27 20:08:34 -0600

    add production webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,78 +3,75 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 // const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
-const config = {
-  context: __dirname,
-  entry: './components/BrowserEntry.jsx',
-  output: {
-    path: path.join(__dirname, 'public'),
-    publicPath: '/public/',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  node: {
-    net: 'empty',
-    dns: 'empty'
-  },
-  stats: "minimal",
-  module: {
-    // preLoaders: [
-
-    // ],
-    rules: [
-      {
-        test: /\.jsx?$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader?sourceMap',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader','sass-loader']
-        })
-      }
+module.exports = function () {
+  return {
+    context: __dirname,
+    entry: './components/BrowserEntry.jsx',
+    output: {
+      path: path.join(__dirname, 'public'),
+      publicPath: '/public/',
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json']
+    },
+    node: {
+      net: 'empty',
+      dns: 'empty'
+    },
+    stats: "minimal",
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          enforce: 'pre',
+          loader: 'eslint-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader?sourceMap',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.json$/,
+          loader: 'json-loader'
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]'
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader?name=fonts/[name].[ext]'
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader','sass-loader']
+          })
+        }
+      ]
+    },
+    devtool: 'source-map',
+    performance: {
+      hints: "warning",
+      maxEntrypointSize: 400000,
+      maxAssetSize: 300000
+    },
+    plugins: [
+      new ExtractTextPlugin('/css/style.css'),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      })
+  /*    ,
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        server: { baseDir: [''] }
+      })*/
     ]
-  },
-  devtool: 'source-map',
-  performance: {
-    hints: "warning",
-    maxEntrypointSize: 400000,
-    maxAssetSize: 300000
-  },
-  plugins: [
-    new ExtractTextPlugin('/css/style.css'),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    })
-/*    ,
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: [''] }
-    })*/
-  ]
+  }
 }
-
-module.exports = config
---
+++ webpack.prod.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+
+const commonConfig = require('./webpack.config.js')
+
+module.exports = function (env) {
+  return webpackMerge(commonConfig(), {
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        compress: {
+          screw_ie8: true
+        },
+        comments: false
+      })
+    ]
+  })
+}
